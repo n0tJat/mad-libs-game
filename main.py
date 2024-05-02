fd451642bd76612e4d82b2cf3beb19d4caf5e52e
n0tJat <janootz1405@gmail.com>
2024-05-02 15:58:10 +0100

    Added game

---
+++ main.py
@@ -0,0 +1,8 @@
+adjective = input("Pick an adjective to describe tech: ")
+number = input("How long has it taken you to break into the industry (in months)? ")
+tech_area = input("What part of Tech would you like to work in? ")
+
+
+print(f"Tech is {adjective}. ")
+print(f"It's taken me {number} of months to break into the industry. ")
+print(f"I am specialising in {tech_area}. ")
